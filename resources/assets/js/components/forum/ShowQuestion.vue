c60d2d57862bc72f62f470f3285f021cb54c6796
sayedme120 <sayedme120@gmail.com>
2020-08-16 04:57:13 +0600

    Front End Question CRUD

--- resources/assets/js/components/forum/ShowQuestion.vue
+++ resources/assets/js/components/forum/ShowQuestion.vue
@@ -12,7 +12,14 @@
                 <v-btn color="teal" dark>5 Replies</v-btn>
             </v-card-title>
             <v-card-text v-html="body"></v-card-text>
-            <v-card-actions></v-card-actions>
+            <v-card-actions v-if="own">
+                <v-btn icon small>
+                    <v-icon color="orange">edit</v-icon>
+                </v-btn>
+                <v-btn icon small @click="destroy">
+                    <v-icon color="red">delete</v-icon>
+                </v-btn>
+            </v-card-actions>
         </v-container>
     </v-card>
 </template>
@@ -21,9 +28,21 @@
 
 export default {
     props:['data'],
+    data(){
+        return {
+            own : User.own(this.data.user_id)
+        }
+    },
     computed:{
         body(){
             return md.parse(this.data.body)
+        }
+    },
+    methods:{
+        destroy(){
+            axios.delete(`/api/question/${this.data.slug}`)
+            .then(res => this.$router.push('/forum'))
+            .catch(error => console.log(error.response.data))
         }
     }
 }
