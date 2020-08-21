bd5b1c1f63f47edc3bc5131cc6a6e754297b32cd
sayedme120 <sayedme120@gmail.com>
2020-08-22 05:06:15 +0600

    modify Reply functionality

---
+++ resources/assets/js/components/reply/replies.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-container>
+        <reply
+            v-for="reply in replies"
+            :key="reply.id"
+            v-if="replies"
+            :data="reply"
+        ></reply>
+    </v-container>
+</template>
+
+<script>
+import Reply from './reply'
+export default {
+    props:['replies'],
+    components:{Reply}
+}
+</script>
+
+<style scoped>
+
+</style>
