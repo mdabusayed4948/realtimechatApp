5e5790bf0726993d94cd88de4a88a05ad57023bd
sayedme120 <sayedme120@gmail.com>
2020-09-10 05:39:29 +0600

    modify something, create something and add sound system

---
+++ resources/assets/js/components/Parallax.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-parallax
+        height="700"
+        src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+    >
+        <v-layout
+            align-center
+            column
+            justify-center
+        >
+            <h1 class="display-3 font-weight-thin mb-3">Real SPA (Laravel & VueJS)</h1>
+            <h4 class="headline">Real Time Chat App</h4>
+        </v-layout>
+    </v-parallax>
+</template>
+
+<script>
+export default {
+name: "Parallex"
+}
+</script>
+
+<style scoped>
+
+</style>
